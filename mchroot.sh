5353547f61a50cf7603f013c8add90de7ee5f65a
Paul Soucy <paul@dev-smart.com>
2015-02-11 00:16:58 -0500

    kill processes before unmounting

--- mchroot.sh
+++ mchroot.sh
@@ -27,6 +27,26 @@ mount -o bind /dev $ENV_ROOT/dev
 
 chroot $ENV_ROOT
 
+#kill all processes in chroot
+
+FOUND=0
+
+for ROOT in /proc/*/root; do
+    LINK=$(readlink $ROOT)
+    if [ "x$LINK" != "x" ]; then
+        if [ "x${LINK:0:${#ENV_ROOT}}" = "x$ENV_ROOT" ]; then
+            # this process is in the chroot...
+            PID=$(basename $(dirname "$ROOT"))
+            echo "killing PID: $PID"
+            kill -9 "$PID"
+            FOUND=1
+        fi
+    fi
+done
+
+echo "waiting for processes to die..."
+sleep 2
+
 #unmount
 for dir in $UNDERLAY_DIRS; do
   umount $ENV_ROOT/$dir
--- unmount.sh
+++ unmount.sh
@@ -3,6 +3,27 @@
 ENV_ROOT="/tmp/mchroot"
 OVERLAY_ROOT="/tmp/mchroot-overlay"
 
+
+#kill all processes in chroot
+
+FOUND=0
+
+for ROOT in /proc/*/root; do
+    LINK=$(readlink $ROOT)
+    if [ "x$LINK" != "x" ]; then
+        if [ "x${LINK:0:${#ENV_ROOT}}" = "x$ENV_ROOT" ]; then
+            # this process is in the chroot...
+            PID=$(basename $(dirname "$ROOT"))
+            echo "killing PID: $PID"
+            kill -9 "$PID"
+            FOUND=1
+        fi
+    fi
+done
+
+echo "waiting for processes to die..."
+sleep 2
+
 UNDERLAY_DIRS="bin sbin etc home lib lib32 lib64 usr var"
 
 for dir in $UNDERLAY_DIRS; do
